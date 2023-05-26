f04926a85959959b8d5eec61063619bee298f945
Mahmudul Hasan Shafin <mahmud.shafin@bidtrix.com>
2023-05-26 14:18:09 +0600

    initial commit

---
+++ src/main/java/com/ubetgpt/betgpt/BetgptApplication.java
@@ -0,0 +1,13 @@
+package com.ubetgpt.betgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetgptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetgptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ubetgpt/betgpt/BetgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubetgpt.betgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetgptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
