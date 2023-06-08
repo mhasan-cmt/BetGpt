9472d92f209acbacd507f2d8f3f3968e8f20611a
Sayed Mahmud Raihan <sayedmahmudraihan@gmail.com>
2023-06-08 14:25:15 +0600

    removed unnccesary

--- src/test/java/com/ubetgpt/betgpt/BetgptApplicationTests.java
+++ src/test/java/com/ubetgpt/betgpt/BetgptApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ubetgpt.betgpt;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BetgptApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.ubetgpt.betgpt;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BetgptApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
